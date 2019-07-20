e3e4a981fe6558c01faf4830eea0ed7a20a77559
Rizwan Khan <riz.insure@gmail.com>
2019-07-20 17:36:17 +0530

    Added Source Code

---
+++ src/com/example/keypair/GenerateKeys.java
@@ -0,0 +1,70 @@
+
+package com.example.keypair;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class GenerateKeys {
+	private KeyPairGenerator	keyGen;
+	private KeyPair				pair;
+	private PrivateKey			privateKey;
+	private PublicKey			publicKey;
+
+	public GenerateKeys(int keylength) throws NoSuchAlgorithmException, NoSuchProviderException {
+		keyGen = KeyPairGenerator.getInstance("RSA");
+		keyGen.initialize(keylength);
+	}
+
+	public void createKeys() {
+		pair = keyGen.generateKeyPair();
+		privateKey = pair.getPrivate();
+		publicKey = pair.getPublic();
+	}
+
+	public PrivateKey getPrivateKey() {
+		return privateKey;
+	}
+
+	public PublicKey getPublicKey() {
+		return publicKey;
+	}
+
+	public void writeToFile(String path, byte[] key) throws IOException {
+		File f = new File(path);
+		f.getParentFile().mkdirs();
+
+		FileOutputStream fos = new FileOutputStream(f);
+		fos.write(key);
+		fos.flush();
+		fos.close();
+	}
+
+	public static void main(String[] args) {
+		GenerateKeys gk;
+		try
+		{
+			System.out.println("Generating Public and Private Key");
+			gk = new GenerateKeys(1024);
+			gk.createKeys();
+			gk.writeToFile("KeyPair/publicKey", gk.getPublicKey().getEncoded());
+			gk.writeToFile("KeyPair/privateKey", gk.getPrivateKey().getEncoded());
+			System.out.println("Key Saved....");
+		}
+		catch (NoSuchAlgorithmException | NoSuchProviderException e)
+		{
+			System.err.println(e.getMessage());
+		}
+		catch (IOException e)
+		{
+			System.err.println(e.getMessage());
+		}
+
+	}
+}
